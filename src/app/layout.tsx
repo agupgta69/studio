5ae254265b16737950baa6ccc03c9221e7810257
Akash <akashumeshgupta@gmail.com>
2026-01-03 20:13:19 +0000

    change whiteboard Insights to Talentbridge Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Footer } from '@/components/layout/footer';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'Whiteboard Insights',
+  title: 'Talentbridge Insights',
   description: 'Insights on business, technology, and design.',
 };
 
